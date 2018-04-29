0c3703df5ebd6119e316fca45255c29ef3e0f941
ujjwal singh <ujjwalsingh15@gmail.com>
2018-04-29 17:07:47 +0530

    working on eliza

---
+++ 1_simple_echobot.py
@@ -0,0 +1,15 @@
+# 1st
+
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+def respond(message):
+    bot_message = "I can hear you! You said: " + message
+    return bot_message
+
+def send_message(message):
+    print(user_template.format(message))
+    response = respond(message)
+    print(bot_template.format(response))
+
+send_message("hello")
---
+++ Final_Chatbot.py
@@ -0,0 +1,42 @@
+# Final Chatbot.
+
+import random
+
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+# Define a dictionary with the predefined responses
+responses = {
+	'question': ["I don't know :(", 'you tell me!'],
+ 	'statement': ['tell me more!',
+  		'why do you think that?',
+  		'how long have you felt this way?',
+  		'I find that extremely interesting',
+  		'can you back that up?',
+  		'oh wow!',
+  		':)']
+}
+
+def send_message(message):
+    print(user_template.format(message))
+    response = respond(message)
+    print(bot_template.format(response))
+
+def respond(message):
+    # Check for a question mark
+    if message.endswith("?"):
+        # Return a random question
+        return random.choice(responses["question"])
+    # Return a random statement
+    return random.choice(responses["statement"])
+
+
+# Send messages ending in a question mark
+send_message("what's today's weather?")
+send_message("what's today's weather?")
+
+# Send messages which don't end with a question mark
+send_message("I love building chatbots")
+send_message("I love building chatbots")
+
+
